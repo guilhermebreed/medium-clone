9919f941e5aa701f5eeeaf211b77ab741f40383b
Guilherme Silva <guilherme.bralves@hotmail.com>
2018-07-27 10:11:43 -0300

    Add state management (Redux)

---
+++ src/redux/reducers/article.js
@@ -0,0 +1,27 @@
+// src/redux/reducers/article.js
+const initialState = {
+    articles: [],
+    article: {}
+}
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case 'LOAD_ARTICLES':
+        return {
+            ...state,
+            articles: action.articles
+        }            
+        case 'VIEW_ARTICLE':
+        return {
+            ...state,
+            article: action.article
+        }
+        case 'CLAP_ARTICLE':
+        let article = Object.assign({}, state.article)
+        return {
+            ...state,
+            article: article
+        }
+        default:
+            return state;
+    }
+}
---
+++ src/redux/reducers/common.js
@@ -0,0 +1,18 @@
+// src/redux/reducers/common.js
+const defaultState = {
+    appName: '',
+    modalMode: false
+};
+
+export default (state = defaultState, action) => {
+    switch (action.type) {
+        case 'TOGGLE_MODAL':
+        console.log(`toggling modal: ${action.modalMode}`)
+        return {
+            ...defaultState,
+            modalMode: action.modalMode
+        }
+        default:
+            return state;
+    }
+};
